ca2825ffebb202033f737336e85c25259aae6edc
B1rtek <b1rtek@fakemail.com>
2022-02-11 00:03:59 +0100

    Implemented Enemy.h and Enemy.cpp

--- Enemy.cpp
+++ Enemy.cpp
@@ -1,4 +1,8 @@
 #include "Enemy.h"
+#include "Board.h"
+#include <algorithm>
+#include <random>
+#include <ctime>
 
 std::pair<char, int> upperField(std::pair<char, int> coords) {
     return std::make_pair(coords.first, coords.second - 1);
@@ -42,8 +46,156 @@ std::vector<std::pair<char, int>> createListOfTangents(std::pair<char, int> sour
     return tangents;
 }
 
+/**
+ * Creates an Enemy object and initializes all its fields
+ * @param hardMode makes enemy use field ranking when choosing a field to shoot at
+ *
+ * Initializes 3 vectors - a vector of undiscovered fields which Enemy will shoot
+ * randomly at, a vector of toShoot fields, which have higher priority than random
+ * targets and are set once a ship is hit, and toMarkAsEmpty vector, which gets \
+ * populated by fields to mark as empty once a ship is hit
+ */
+Enemy::Enemy(bool hardMode) {
+    for (auto &field: returnAllFieldCoordinates()) {
+        this->undiscovered.push_back(field);
+        this->undiscoveredSet.insert(field);
+    }
+    this->hardMode = hardMode;
+}
 
+/**
+ * @brief Chooses a field based on the probability of it having a ship
+ * @return coordinates of a chosen field
+ *
+ * Creates a vector of fields sorted by the maximum length of a ship that can be
+ * located there, and then returns coordinates of one with the highest score
+ */
+std::pair<char, int> Enemy::rankFieldsAndChoose() {
+    std::vector<std::pair<int, std::pair<char, int>>> rankList;
+    for (auto &field: this->undiscovered) {
+        int scoreVert = 1, scoreHoriz = 1;
+        std::pair<char, int> current = field;
+        current = upperField(current);
+        while (this->undiscoveredSet.count(current)) {
+            scoreVert++;
+            current = upperField(current);
+        }
+        current = field;
+        current = lowerField(current);
+        while (this->undiscoveredSet.count(current)) {
+            scoreVert++;
+            current = lowerField(current);
+        }
+        current = field;
+        current = rightField(current);
+        while (this->undiscoveredSet.count(current)) {
+            scoreHoriz++;
+            current = rightField(current);
+        }
+        current = field;
+        current = leftField(current);
+        while (this->undiscoveredSet.count(current)) {
+            scoreHoriz++;
+            current = leftField(current);
+        }
+        rankList.emplace_back(std::max(scoreVert, scoreHoriz), field);
+        std::sort(rankList.begin(), rankList.end());
+        int maxScore = rankList.back().first;
+        std::vector<std::pair<char, int>> bestFields;
+        for (unsigned i = rankList.size() - 1; i >= 0; i--) {
+            if (rankList[i].first != maxScore) {
+                break;
+            }
+            bestFields.push_back(rankList[i].second);
+        }
+        std::mt19937 gen(time(nullptr));
+        std::uniform_int_distribution<> dist(0, bestFields.size() - 1);
+        return bestFields[dist(gen)];
+    }
+}
 
+/**
+ * @brief Removes the given value from the vector-set undiscovered combo.
+ * @param value value to remove
+ */
+void Enemy::undiscoveredRemove(std::pair<char, int> value) {
+    this->undiscovered.erase(std::remove(this->undiscovered.begin(), this->undiscovered.end(), value),
+                             this->undiscovered.end());
+    this->undiscoveredSet.erase(value);
+}
 
+/**
+ * @brief Chooses a field that will be shot at
+ * @return field coordinates of a chosen field
+ */
+std::pair<char, int> Enemy::shoot() {
+    while (!this->toShoot.empty()) {
+        std::pair<char, int> chosen = this->toShoot[0];
+        this->toShoot.erase(std::remove(this->toShoot.begin(), this->toShoot.end(), chosen), this->toShoot.end());
+        if (this->undiscoveredSet.count(chosen)) {
+            this->undiscoveredRemove(chosen);
+            this->lastTarget = chosen;
+            return chosen;
+        }
+    }
+    std::pair<char, int> chosen;
+    if (this->hardMode) {
+        chosen = this->rankFieldsAndChoose();
+    } else {
+        std::mt19937 gen(time(nullptr));
+        std::uniform_int_distribution<> dist(0, this->undiscovered.size() - 1);
+        chosen = this->undiscovered[dist(gen)];
+    }
+    this->undiscoveredRemove(chosen);
+    this->lastTarget = chosen;
+    return chosen;
+}
 
+/**
+ * @brief Appends coordinates of fields that can't have any ships to the
+ * toMarkAsEmpty vector and puts new targets on the toShoot vector
+ */
+void Enemy::reactToHit() {
+    std::vector<std::pair<char, int>> toMarkAsEmptyList = createListOfTangents(this->lastTarget);
+    for (auto &target: toMarkAsEmptyList) {
+        if (this->undiscoveredSet.count(target)) {
+            this->undiscoveredRemove(target);
+            this->toMarkAsEmpty.push_back(target);
+        }
+    }
+    std::vector<std::pair<char, int>> toShootList = createListOfAdherent(this->lastTarget);
+    std::mt19937 gen(time(nullptr));
+    std::shuffle(toShootList.begin(), toShootList.end(), gen);
+    for (auto &target: toShootList) {
+        if (this->undiscoveredSet.count(target)) {
+            this->toShoot.push_back(target);
+        }
+    }
+}
 
+/**
+ * @brief Appends all the remaining unmarked fields around a sunken ship to the
+ * toMarkAsEmpty vector
+ */
+void Enemy::reactToSink() {
+    std::vector<std::pair<char, int>> toMarkAsEmptyList = createListOfAdherent(this->lastTarget);
+    for (auto &target: toMarkAsEmptyList) {
+        if (this->undiscoveredSet.count(target)) {
+            this->undiscoveredRemove(target);
+            this->toMarkAsEmpty.push_back(target);
+        }
+    }
+    for (auto &target: this->toShoot) {
+        this->toMarkAsEmpty.push_back(target);
+    }
+    this->toShoot.clear();
+}
+
+/**
+ * @return a vector of fields to mark as empty after a move
+ */
+std::vector<std::pair<char, int>> Enemy::markAsEmpty() {
+    std::vector<std::pair<char, int>> toMarkAsEmptyList = this->toMarkAsEmpty;
+    this->toMarkAsEmpty.clear();
+    return toMarkAsEmptyList;
+}
--- Enemy.h
+++ Enemy.h
@@ -3,6 +3,7 @@
 
 #include <utility>
 #include <vector>
+#include <set>
 
 std::pair<char, int> upperField(std::pair<char, int> coords);
 
@@ -15,5 +16,32 @@ std::pair<char, int> rightField(std::pair<char, int> coords);
 std::vector<std::pair<char, int>> createListOfAdherent(std::pair<char, int> source);
 
 std::vector<std::pair<char, int>> createListOfTangents(std::pair<char, int> source);
+
+/**
+ * @brief Class representing the computer opponent
+ */
+class Enemy {
+    std::vector<std::pair<char, int>> undiscovered;
+    std::set<std::pair<char, int>> undiscoveredSet;
+    std::vector<std::pair<char, int>> toShoot;
+    std::vector<std::pair<char, int>> toMarkAsEmpty;
+    std::pair<char, int> lastTarget;
+    bool hardMode;
+
+    std::pair<char, int> rankFieldsAndChoose();
+
+    void undiscoveredRemove(std::pair<char, int> value);
+
+public:
+    explicit Enemy(bool hardMode = false);
+
+    std::pair<char, int> shoot();
+
+    void reactToHit();
+
+    void reactToSink();
+
+    std::vector<std::pair<char, int>> markAsEmpty();
+};
 
 #endif //BATTLESHIP_CPP_ENEMY_H
